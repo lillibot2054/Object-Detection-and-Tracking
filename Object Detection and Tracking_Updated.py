a85df70dfdb95c11f7d739e7a0ab13ca7e9fea6e
DHEEPAK.G <lillibot2054@gmail.com>
2025-02-26 13:32:18 +0530

    Object Detection and Tracking-Updated.py

--- Object Detection and Tracking_Updated.py
+++ Object Detection and Tracking_Updated.py
@@ -1,7 +1,12 @@
 import os
-os.environ['KMP_DUPLICATE_LIB_OK']='TRUE'
-
+# Fix potential library conflicts
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
+# Import YOLO from Ultralytics
 from ultralytics import YOLO
+# Load the YOLOv8 model (medium version)
 model = YOLO('yolov8m.pt')
-
-results = model.track(source=0, show=True, tracker="bytetrack.yaml")
+# Perform object tracking on the given video
+# 'source' specifies the input video file
+# 'show=True' displays the output in a window
+# 'tracker="bytetrack.yaml"' enables the ByteTrack algorithm for tracking
+results = model.track(source="Car_Video_Sample.mp4", show=True, tracker="bytetrack.yaml")
